6f0587dfd9f29f9ee72d9ef2fbda78f27ff77187
Sibam <sibampaulsp2@gmail.com>
2024-05-19 22:41:44 +0530

    Making necessary Folders for Backend

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const PORT = 4444;
-const hbs = require('hbs');
+const path=require('path');
+const express =require('express')
+const app=express();
+const PORT=4444;
+ 
+app.set('view engine','hbs')
+app.use(express.urlencoded({extended:true}));
+// app.use(express.static(path.join(__dirname,'')))
 
-app.use(express.static(__dirname + '/public'));
-hbs.registerPartials(path.join(__dirname + '/views/partials'));
-app.set('view engine', 'hbs');
-app.use(express.urlencoded({ extended: true }));
+ 
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-// dj wale babu
-
-
-app.listen(PORT, () => {
-    console.log(`http://localhost:` + PORT);
+ 
+ 
+app.listen(PORT,()=>{
+console.log(`http://localhost:`+PORT)
 });
